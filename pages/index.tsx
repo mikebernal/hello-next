68f7f2bea0e51448b196621b86b5e538e3fe27e3
Mike Bernal <mikebernal.me@gmail.com>
2021-02-01 23:59:46 +0800

    style: use scss and install sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
+import styles from '../styles/Home.module.scss'
+
 export default function Home() {
   return (
-    <h1>Hello world!</h1>
+    <div>
+        <h1 className={styles.green}>Hello world</h1>
+    </div>
   )
 }
